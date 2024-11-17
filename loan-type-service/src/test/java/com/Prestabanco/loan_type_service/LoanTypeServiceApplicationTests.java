b24c1e873356cc9d3c924c380383350f66e46daf
Josue Quileñan <josue.quilenan@usach.cl>
2024-11-17 15:49:44 -0300

    Added the first microservice, and uptdated the config-service to use the java dotenv dependency to use the .env credentials correctly.

---
+++ loan-type-service/src/test/java/com/Prestabanco/loan_type_service/LoanTypeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prestabanco.loan_type_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanTypeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
