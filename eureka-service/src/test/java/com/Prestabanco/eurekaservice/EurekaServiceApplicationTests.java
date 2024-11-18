70f81ce0805540ae2af4b2a72c3ec1962c470a79
Josue Quileñan <josue.quilenan@usach.cl>
2024-11-17 23:45:39 -0300

    Modified the versions on the pom.xml files so everything works together.

---
+++ eureka-service/src/test/java/com/Prestabanco/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prestabanco.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
