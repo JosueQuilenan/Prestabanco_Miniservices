6f08964114dac9dbac7654ebfebadc549c67adca
Josue Quileñan <josue.quilenan@usach.cl>
2024-12-05 01:57:14 -0300

    Added the deployment for the frontend and little changes on some microservices deployment archives.

--- PrestaBanco_Frontend/src/http-common.js
+++ PrestaBanco_Frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const prestaBancoBackendServer = import.meta.env.VITE_PRESTABANCO_BACKEND_SERVER;
-const prestaBancoBackendPort = import.meta.env.VITE_PRESTABANCO_BACKEND_PORT;
+const prestaBancoBackendServer = import.meta.env.VITE_PRESTABANCO_BACKEND_SERVER || 'gateway-server-service';
+const prestaBancoBackendPort = import.meta.env.VITE_PRESTABANCO_BACKEND_PORT || '8080';
 
 export default axios.create({
     baseURL: `http://${prestaBancoBackendServer}:${prestaBancoBackendPort}`,
